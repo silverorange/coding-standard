35edeeada2153affec56f4015030ec2494243565
Michael Gauthier <mike@silverorange.com>
2017-02-12 23:08:56 -0400

    fix cs

--- src/SilverorangeLegacy/Sniffs/Whitespace/OperatorSpacingSniff.php
+++ src/SilverorangeLegacy/Sniffs/Whitespace/OperatorSpacingSniff.php
@@ -34,7 +34,7 @@
 
 class SilverorangeLegacy_Sniffs_WhiteSpace_OperatorSpacingSniff extends Squiz_Sniffs_Whitespace_OperatorSpacingSniff
 {
-    public function process (PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
 
